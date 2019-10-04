90b249a64d02474cae68483a51f73d2043952b30
maymaying <1900750175@qq.com>
2019-10-04 08:53:33 +0800

    190929-BooksSort-图书排序

---
+++ 190929--图书排序/src/Book.java
@@ -0,0 +1,35 @@
+public class Book implements Comparable<Book> {
+    String ISBN;
+    String title;
+    String author;
+    int price;
+    int sales;
+    int comments;
+
+    //Alt+Insert 快捷键
+    public Book(String ISBN, String title, String author, int price, int sales, int comments) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.sales = sales;
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "ISBN='" + ISBN + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", sales=" + sales +
+                ", comments=" + comments +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return ISBN.compareTo(o.ISBN);
+    }
+}
